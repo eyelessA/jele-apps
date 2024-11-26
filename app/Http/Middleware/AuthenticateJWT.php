dec890920669174039675003d9ad84d7e17998cd
olegbugaev <bugaev0leg@yandex.ru>
2024-11-26 18:20:17 +0400

    feat: add middleware

---
+++ app/Http/Middleware/AuthenticateJWT.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Services\User\JWTService;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthenticateJWT
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Closure(Request): (Response) $next
+     */
+    private JWTService $jwtService;
+
+    public function __construct(JWTService $jwtService)
+    {
+        $this->jwtService = $jwtService;
+    }
+
+    public function handle($request, Closure $next)
+    {
+        $token = $request->header('Bearer');
+
+        if (!$token) {
+            return response()->json(['error' => 'Token not provided'], 401);
+        }
+
+        $payload = $this->jwtService->verifyJWT($token, env('JWT_SECRET'));
+
+        if (!$payload || !is_array($payload) || $payload['exp'] < time()) {
+            return response()->json(['error' => 'Token invalid or expired'], 401);
+        }
+
+        $request->attributes->add(['user' => $payload]);
+
+        return $next($request);
+    }
+}
